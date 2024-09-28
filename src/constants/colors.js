32e810ff846288865b3619e177355b9501399ed2
Suhail0206327 <122574657+suhail3728@users.noreply.github.com>
2024-09-27 18:22:20 -0700

    Create new decision screen for the users

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,6 +3,9 @@ const Colors = {
     green:'#3ABEA9', 
     gray: '#F3F3F3',
     border: '#DAD9D7',
+    orange: '#FE6809',
+    white: '#FEFEFE',
+    darkGray:'#4F4F4F',
   };
   
   export default Colors;
--- src/screens/NewUserScreen.tsx
+++ src/screens/NewUserScreen.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { NewUserProps } from '../types/types';
-import { IconButton } from 'react-native-paper';
-function NewUserScreen  ({ navigation }:NewUserProps) {
+import {View, Text, Image,  StyleSheet} from 'react-native';
+import {NewUserProps} from '../types/types';
+import {IconButton} from 'react-native-paper';
+import CustomButton from '../components/CustomButton';
+import Colors from '../constants/colors';
+
+function NewUserScreen({navigation}: NewUserProps) {
   return (
     <View style={styles.container}>
-      
-     
-      <IconButton 
-        icon="close" 
-        size={24} 
-        style={styles.closeButton} 
-        onPress={() => navigation.goBack()} 
+      <IconButton
+        icon="close"
+        size={24}
+        style={styles.closeButton}
+        onPress={() => navigation.goBack()}
       />
-     
-      <Text style={styles.title}>New User Screen</Text>
-    </View>
 
+<Text style={styles.text}>So glad you're here! How can we help you?</Text>
+<Image
+        source={require('../assets/images/newuser.png')}
+        style={styles.image}
+      />
+      <CustomButton
+        backgroundColor={Colors.orange}
+        onPress={() => {
+          console.log('create a new team');
+        }}
+        textColor={Colors.white}
+        borderColor={Colors.orange}
+        title={'Create a new team'}></CustomButton>
+         <CustomButton
+        backgroundColor={Colors.orange}
+        onPress={() => {
+          console.log('Join an existing team');
+        }}
+        textColor={Colors.white}
+        borderColor={Colors.orange}
+        title={'Join an existing team'}></CustomButton>
+    </View>
   );
 }
 
@@ -25,13 +45,29 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0',
+    backgroundColor: Colors.white,
+  },
+  text: {
+    fontSize:27,
+    fontWeight:'bold',
+    padding:40,
+    color: Colors.darkGray,
+    textAlign:'center',
   },
   closeButton: {
     position: 'absolute',
     top: 40,
     left: 20,
     padding: 10,
+  },
+  image: {
+
+    width: 300,
+    height: 300,
+    marginTop:10,
+    marginBottom:70,
+
+
   },
   closeButtonText: {
     fontSize: 24,
